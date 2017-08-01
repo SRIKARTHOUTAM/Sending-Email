53bddb5c232726d6e1558fd9fdaa91b92754769c
SRIKARTHOUTAM <srikarthoutam@gmail.com>
2017-08-01 15:11:19 -0400

    First Commit

---
+++ SendingEmail/Email.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Email.aspx.cs" Inherits="SendingEmail.Email" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div >
+        From Email:
+        <asp:TextBox ID="from" runat="server"></asp:TextBox><br />
+         Password:
+        <asp:TextBox ID="pwd" textmode="password" runat="server"></asp:TextBox><br />
+        To Email ID:
+        <asp:TextBox ID="to" runat="server"></asp:TextBox><br />
+        Subject:
+        <asp:TextBox ID="subject" runat="server"></asp:TextBox><br />
+        Message:
+        <asp:TextBox ID="body" textmode="MultiLine" runat="server"></asp:TextBox><br />
+
+        <asp:Button ID="send" runat="server" Text="Send" OnClick="sendbtn_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ SendingEmail/Email.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SendingEmail
+{
+    public partial class Email : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+             
+        }
+
+        protected void sendbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MailMessage message = new MailMessage(from.Text, to.Text, subject.Text, body.Text);
+                SmtpClient client = new SmtpClient("smtp.gmail.com",587);
+                client.EnableSsl = true;
+                client.Credentials = new System.Net.NetworkCredential(from.Text, pwd.Text);
+                client.Send(message);
+
+            }
+            catch(Exception)
+            {
+         
+            }
+        }
+    }
+}
